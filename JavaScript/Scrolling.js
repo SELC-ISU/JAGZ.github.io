b4ce63574dfc9f942b7c69fc544e3fb7e73aca63
Zach <zwingert@iastate.edu>
2020-03-11 10:11:16 -0500

    Zach's page

--- JavaScript/Scrolling.js
+++ JavaScript/Scrolling.js
@@ -1,1 +1,11 @@
-alert(window.pageYOffset);
+"use strict";
+var prevScrollPos = window.pageYOffset;
+window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollPos > currentScrollPos) {
+        document.getElementById("zach").style.top = "0";
+    } else {
+        document.getElementById("zach").style.top = "-50px";
+    }
+    prevScrollPos = currentScrollPos;
+}
